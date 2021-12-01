187161acbecbde251d7c7ef649baff19e5056b9f
Tomas Plavskis <tomasplavskis@gmail.com>
2021-12-01 19:54:57 +0200

    Added Day 1

---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,6 @@
+﻿using AdventOfCode2021;
+using AdventOfCode2021.Enumerations;
+
+var runmode = RunMode.Day1Part2;
+
+RunModeHandler.Execute(runmode);
---
+++ AdventOfCode2021/RunModeHandler.cs
@@ -0,0 +1,155 @@
+﻿using AdventOfCode2021.Days;
+using AdventOfCode2021.Enumerations;
+
+namespace AdventOfCode2021
+{
+    internal static class RunModeHandler
+    {
+        internal static void Execute(RunMode runMode)
+        {
+            switch (runMode)
+            {
+                case RunMode.Day1Part1:
+                    Day1Part1Handler();
+                    break;
+                case RunMode.Day1Part2:
+                    Day1Part2Handler();
+                    break;
+                case RunMode.Day2Part1:
+                    break;
+                case RunMode.Day2Part2:
+                    break;
+                case RunMode.Day3Part1:
+                    break;
+                case RunMode.Day3Part2:
+                    break;
+                case RunMode.Day4Part1:
+                    break;
+                case RunMode.Day4Part2:
+                    break;
+                case RunMode.Day5Part1:
+                    break;
+                case RunMode.Day5Part2:
+                    break;
+                case RunMode.Day6Part1:
+                    break;
+                case RunMode.Day6Part2:
+                    break;
+                case RunMode.Day7Part1:
+                    break;
+                case RunMode.Day7Part2:
+                    break;
+                case RunMode.Day8Part1:
+                    break;
+                case RunMode.Day8Part2:
+                    break;
+                case RunMode.Day9Part1:
+                    break;
+                case RunMode.Day9Part2:
+                    break;
+                case RunMode.Day10Part1:
+                    break;
+                case RunMode.Day10Part2:
+                    break;
+                case RunMode.Day11Part1:
+                    break;
+                case RunMode.Day11Part2:
+                    break;
+                case RunMode.Day12Part1:
+                    break;
+                case RunMode.Day12Part2:
+                    break;
+                case RunMode.Day13Part1:
+                    break;
+                case RunMode.Day13Part2:
+                    break;
+                case RunMode.Day14Part1:
+                    break;
+                case RunMode.Day14Part2:
+                    break;
+                case RunMode.Day15Part1:
+                    break;
+                case RunMode.Day15Part2:
+                    break;
+                case RunMode.Day16Part1:
+                    break;
+                case RunMode.Day16Part2:
+                    break;
+                case RunMode.Day17Part1:
+                    break;
+                case RunMode.Day17Part2:
+                    break;
+                case RunMode.Day18Part1:
+                    break;
+                case RunMode.Day18Part2:
+                    break;
+                case RunMode.Day19Part1:
+                    break;
+                case RunMode.Day19Part2:
+                    break;
+                case RunMode.Day20Part1:
+                    break;
+                case RunMode.Day20Part2:
+                    break;
+                case RunMode.Day21Part1:
+                    break;
+                case RunMode.Day21Part2:
+                    break;
+                case RunMode.Day22Part1:
+                    break;
+                case RunMode.Day22Part2:
+                    break;
+                case RunMode.Day23Part1:
+                    break;
+                case RunMode.Day23Part2:
+                    break;
+                case RunMode.Day24Part1:
+                    break;
+                case RunMode.Day24Part2:
+                    break;
+                case RunMode.Day25Part1:
+                    break;
+                case RunMode.Day25Part2:
+                    break;
+                case RunMode.Day26Part1:
+                    break;
+                case RunMode.Day26Part2:
+                    break;
+                case RunMode.Day27Part1:
+                    break;
+                case RunMode.Day27Part2:
+                    break;
+                case RunMode.Day28Part1:
+                    break;
+                case RunMode.Day28Part2:
+                    break;
+                case RunMode.Day29Part1:
+                    break;
+                case RunMode.Day29Part2:
+                    break;
+                case RunMode.Day30Part1:
+                    break;
+                case RunMode.Day30Part2:
+                    break;
+                case RunMode.Day31Part1:
+                    break;
+                case RunMode.Day31Part2:
+                    break;
+            }
+
+
+        }
+
+        private static void Day1Part1Handler()
+        {
+            var handler = new Day1Part1();
+            handler.Execute();
+        }
+
+        private static void Day1Part2Handler()
+        {
+            var handler = new Day1Part2();
+            handler.Execute();
+        }
+    }
+}
